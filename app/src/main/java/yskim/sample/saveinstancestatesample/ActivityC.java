9d0e790a9efda382ad0256b607db02191e75cccb
mingmonth <ys9922@naver.com>
2019-12-16 16:59:13 +0900

    LED 별 INPUT 처리 샘플 소스 정리

--- app/src/main/java/yskim/sample/saveinstancestatesample/ActivityC.java
+++ app/src/main/java/yskim/sample/saveinstancestatesample/ActivityC.java
@@ -17,6 +17,9 @@ import yskim.sample.saveinstancestatesample.data.LED;
 
 public class ActivityC extends AppCompatActivity {
 
+    private static final int LED_NUM = 6;
+    private static final int INPUT_NUM = 9;
+
     @BindView(R.id.current_led_name)
     TextView currentLedName;
 
@@ -24,32 +27,33 @@ public class ActivityC extends AppCompatActivity {
     HashMap<Integer, ImageView> ledInputValueHashMap = new HashMap<Integer, ImageView>();
     HashMap<Integer, ImageView> inputHashMap = new HashMap<Integer, ImageView>();
 
-    LED[] leds = new LED[6];
+    LED[] leds = new LED[LED_NUM];
     int ledIndex = 0;
     int inputIndex = 0;
 
     private void initializeData() {
-        for(int i = 0; i < leds.length; i++) {
+        for(int i = 0; i < LED_NUM; i++) {
             leds[i] = new LED();
+            // 순차적인 리소스를 처리하기 위한 코드.
             int resId = this.getResources().getIdentifier("led" + (i + 1), "id", this.getPackageName());
-            ledHashMap.put(i, (ImageView) findViewById(resId));
+            ledHashMap.put(i, findViewById(resId));
             int resId2 = this.getResources().getIdentifier("ic_led" + (i + 1) + "_24px", "drawable", this.getPackageName());
             ledHashMap.get(i).setImageResource(resId2);
             int resId3 = this.getResources().getIdentifier("led" + (i + 1) + "_input_value", "id", this.getPackageName());
-            ledInputValueHashMap.put(i, (ImageView) findViewById(resId3));
+            ledInputValueHashMap.put(i, findViewById(resId3));
         }
 
-        for(int j = 0; j < 9; j ++) {
+        for(int j = 0; j < INPUT_NUM; j ++) {
             int resId = this.getResources().getIdentifier("input" + (j + 1), "id", this.getPackageName());
-            inputHashMap.put(j, (ImageView) findViewById(resId));
+            inputHashMap.put(j, findViewById(resId));
             inputHashMap.get(j).setImageResource(R.drawable.ic_uncheck_24px);
         }
     }
 
-    private void displayData() {
+    private void showDataLog() {
         INPUT[] inputs = null;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 9; j++) {
+        for(int i = 0; i < LED_NUM; i++) {
+            for(int j = 0; j < INPUT_NUM; j++) {
                 inputs = leds[i].getInputs();
                 Debug.loge(new Exception(), "LED [ " + i + " ] / INPUT [ " + j +" ] / CHK [ " + ((inputs[j].isChecked() == false) ? "F" : "T") + " ] / DIS [ " + ((inputs[j].isDisabled() == false) ? "F" : "T") + " ]");
             }
@@ -59,9 +63,7 @@ public class ActivityC extends AppCompatActivity {
 
     private void setImageResourceToInput(INPUT[] inputs) {
 
-        //int resId = this.getResources().getIdentifier("ic_filter_" + (j + 1) + "_24px", "drawable", this.getPackageName());
-
-        for(int i = 0; i < inputs.length; i++) {
+        for(int i = 0; i < INPUT_NUM; i++) {
             if(inputs[i].isDisabled()) {
                 // set disable image
                 // non-clickable
@@ -71,16 +73,13 @@ public class ActivityC extends AppCompatActivity {
                 // set checked image
                 inputHashMap.get(i).setImageResource(R.drawable.ic_check_24px);
                 inputHashMap.get(i).setClickable(true);
-            } else {// if(!inputs[i].isDisabled() || !inputs[i].isChecked()) {
+            } else {
                 // clickable
                 // set enable image, unchecked image
                 // clickable
                 inputHashMap.get(i).setImageResource(R.drawable.ic_uncheck_24px);
                 inputHashMap.get(i).setClickable(true);
             }
-//            else if(!inputs[i].isChecked()) {
-//                // set unchecked image
-//            }
         }
     }
 
@@ -101,8 +100,8 @@ public class ActivityC extends AppCompatActivity {
         // 선택되지 않았는데 value 가 있으면 gray
         // value 가 없으면 검정색
         INPUT[] inputs = null;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 9; j++) {
+        for(int i = 0; i < LED_NUM; i++) {
+            for(int j = 0; j < INPUT_NUM; j++) {
                 inputs = leds[i].getInputs();
                 if(i == ledIndex) {
                     if(inputs[j].isChecked()) {
@@ -130,25 +129,9 @@ public class ActivityC extends AppCompatActivity {
         setImageResourceToLED(ledIndex, inputIndex, inputs);
     }
 
-    private void displayUI() {
-        INPUT[] inputs = null;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 9; j++) {
-                inputs = leds[i].getInputs();
-                Debug.loge(new Exception(), "LED [ " + i + " ] / INPUT [ " + j +" ] / CHK [ " + ((inputs[j].isChecked() == false) ? "F" : "T") + " ] / DIS [ " + ((inputs[j].isDisabled() == false) ? "F" : "T") + " ]");
-                if(i == 0) {
-                    if(j == 0) {
-                        //setImageResourceToLED(inputs[j], j);
-                    }
-                }
-            }
-            Debug.logd(new Exception(), "");
-        }
-    }
-
     private int getCheckedIndex(int ledIndex) {
         INPUT[] inputs = leds[ledIndex].getInputs();
-        for(int j = 0; j < inputs.length; j++) {
+        for(int j = 0; j < INPUT_NUM; j++) {
             if(inputs[j].isChecked()) {
                 return j;
             }
@@ -191,11 +174,10 @@ public class ActivityC extends AppCompatActivity {
         int checkedIndex = getCheckedIndex(ledIndex);
 
         Debug.logd(new Exception(), "checkedIndex: " + checkedIndex);
-
         setCheckValue(ledIndex, inputIndex);
 
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 9; j++) {
+        for(int i = 0; i < LED_NUM; i++) {
+            for(int j = 0; j < INPUT_NUM; j++) {
                 if(i == ledIndex) {
                     if(j != inputIndex) {
                         // 하나의 input 값 씩 선택되야 하므로 선택된 값을 제외하고 리셋.
@@ -224,109 +206,30 @@ public class ActivityC extends AppCompatActivity {
         setContentView(R.layout.activity_c);
         ButterKnife.bind(this);
         initializeData();
-        //Debug.logd(new Exception(), "" + leds[0]);
     }
 
     @OnClick({R.id.led1, R.id.led3, R.id.led5, R.id.led2, R.id.led4, R.id.led6, R.id.input1, R.id.input4, R.id.input7, R.id.input2, R.id.input5, R.id.input8, R.id.input3, R.id.input6, R.id.input9})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.led1:
-                ledIndex = 0;
-                currentLedName.setText("LED1");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.led2:
-                ledIndex = 1;
-                currentLedName.setText("LED2");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.led3:
-                ledIndex = 2;
-                currentLedName.setText("LED3");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.led4:
-                ledIndex = 3;
-                currentLedName.setText("LED4");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.led5:
-                ledIndex = 4;
-                currentLedName.setText("LED5");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.led6:
-                ledIndex = 5;
-                currentLedName.setText("LED6");
-                //setBackgroundToLED(ledIndex);
-                break;
-            case R.id.input1:
-                inputIndex = 0;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input2:
-                inputIndex = 1;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input3:
-                inputIndex = 2;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input4:
-                inputIndex = 3;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input5:
-                inputIndex = 4;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input6:
-                inputIndex = 5;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input7:
-                inputIndex = 6;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input8:
-                inputIndex = 7;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
-            case R.id.input9:
-                inputIndex = 8;
-                Debug.logd(new Exception(), "led Index: " + ledIndex);
-                Debug.logd(new Exception(), "input Index: " + inputIndex);
-                //setCheckValue(ledIndex, inputIndex);
-                checkData(ledIndex, inputIndex);
-                break;
+            case R.id.led1: ledIndex = 0; break;
+            case R.id.led2: ledIndex = 1; break;
+            case R.id.led3: ledIndex = 2; break;
+            case R.id.led4: ledIndex = 3; break;
+            case R.id.led5: ledIndex = 4; break;
+            case R.id.led6: ledIndex = 5; break;
+            case R.id.input1: inputIndex = 0; checkData(ledIndex, inputIndex); break;
+            case R.id.input2: inputIndex = 1; checkData(ledIndex, inputIndex); break;
+            case R.id.input3: inputIndex = 2; checkData(ledIndex, inputIndex); break;
+            case R.id.input4: inputIndex = 3; checkData(ledIndex, inputIndex); break;
+            case R.id.input5: inputIndex = 4; checkData(ledIndex, inputIndex); break;
+            case R.id.input6: inputIndex = 5; checkData(ledIndex, inputIndex); break;
+            case R.id.input7: inputIndex = 6; checkData(ledIndex, inputIndex); break;
+            case R.id.input8: inputIndex = 7; checkData(ledIndex, inputIndex); break;
+            case R.id.input9: inputIndex = 8; checkData(ledIndex, inputIndex); break;
         }
-        //displayUI();
+        currentLedName.setText("LED" + (ledIndex + 1));
         setBackgroundToLED(ledIndex);
-        displayData();
+        //showDataLog();
         displayUI(ledIndex, inputIndex);
     }
 }
